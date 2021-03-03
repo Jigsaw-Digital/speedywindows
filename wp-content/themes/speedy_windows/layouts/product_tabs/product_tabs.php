b29b4d01b2bcb9ca6c20e134709826c3dfd08537
gottedev <49015820+gottedev@users.noreply.github.com>
2021-03-03 16:35:49 +0000

    [added] product tabs layout

--- wp-content/themes/speedy_windows/layouts/product_tabs/product_tabs.php
+++ wp-content/themes/speedy_windows/layouts/product_tabs/product_tabs.php
@@ -6,6 +6,11 @@
  $main_heading = get_sub_field('main_heading');
  $sub_heading = get_sub_field('sub_heading'); 
  $product_detail = get_sub_field('product_detail');
+ $product_finishing = get_sub_field('product_finishing');
+ $multi_variant_product = get_sub_field('multi_variant_product');
+ $image_content_v2 = get_sub_field('image_content_v2');
+ $image_content_repeat = get_sub_field('image_content_repeat');
+ $tables_image = get_sub_field('tables_image');
 //  $text_content = get_sub_field('text_content');
 // $image = get_sub_field('image');
 // $layout_direction = get_sub_field('layout_direction');
@@ -241,11 +246,178 @@
                                 </div>
                             </div>
                             <?php } ?>
+                            <?php if($product_detail['color_images']) { ?>
+                            <div class="w-full justify-evenly ml-4">
+                                <div
+                                    class="w-full justify-center lg:justify-start lg:w-2/4 flex flex-wrap mt-8">
+                                    <?php foreach($product_detail['colors'] as $itemColor) { ?>
+                                    <div class="">
+                                        <p><?php echo $itemColor['color'] ?></p>
+                                    </div>
+                                    <?php } ?>
+                                </div>
+                            </div>
+                            <?php } ?>
                             <?php if($product_detail['bottom_border']=="show") {?>
                             <div
                                 class="border-b-4 border-dashed border-primary my-14">
                             </div>
                             <?php }?>
+                        </div>
+                        <div class="w-full">
+                            <?php if($product_finishing['product_finishing']) { ?>
+                            <div class="flex flex-wrap justify-between">
+                                <?php foreach($product_finishing['product_finishing'] as $finishing_item) {?>
+                                <div
+                                    class="w-full md:w-45-percent lg:w-30-percent flex flex-col">
+                                    <div>
+                                        <img src="<?php echo $finishing_item['finishing_image']['url'] ?>"
+                                            alt="">
+                                    </div>
+                                    <div class="mt-2">
+                                        <p class="text-2xl">
+                                            <?php echo $finishing_item['finishing_name'] ?>
+                                        </p>
+                                    </div>
+                                    <div class="mt-2">
+                                        <p class="">
+                                            <?php echo $finishing_item['finishing_description'] ?>
+                                        </p>
+                                    </div>
+                                </div>
+                                <?php } ?>
+                            </div>
+                            <?php if($product_finishing['bottom_border']=="show") {?>
+                            <div
+                                class="border-b-4 border-dashed border-primary my-14">
+                            </div>
+                            <?php }?>
+                            <?php } ?>
+                        </div>
+
+                        <div class="w-full">
+                            <?php if($multi_variant_product) { ?>
+                            <?php foreach($multi_variant_product as $product) { ?>
+                            <div class="w-full">
+                                <div
+                                    class='w-full flex flex-col <?php echo $product['layout_direction']=="left"?"lg:flex-row":'lg:flex-row-reverse' ?> justify-evenly'>
+                                    <div
+                                        class="w-full lg:w-2/5 flex flex-col items-center lg:items-start">
+                                        <div>
+                                            <img class=""
+                                                src="<?php echo $product['image']['url'] ?>"
+                                                alt="" />
+                                        </div>
+
+                                    </div>
+                                    <div
+                                        class="w-full lg:w-1/2 flex items-start justify-around flex-col ">
+                                        <?php foreach($product['text_content'] as $item) {?>
+                                        <?php if($item['heading']) { ?>
+                                        <div>
+                                            <h2 class="text-2xl">
+                                                <?php echo $item['heading'] ?>
+                                            </h2>
+                                        </div>
+                                        <?php } ?>
+                                        <?php if($item['paragraph']) { ?>
+                                        <div>
+                                            <p class="text-lg mt-6">
+                                                <?php echo $item['paragraph'] ?>
+                                            </p>
+                                        </div>
+                                        <?php } ?>
+
+                                        <?php if($item['link']) { ?>
+                                        <div>
+                                            <div
+                                                class="mt-6 px-4 py-3 rounded-3xl bg-primary_red">
+                                                <a href="#">
+                                                    <?php echo $item['link']['title'] ?>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <?php } ?>
+
+                                        <?php } ?>
+                                    </div>
+
+                                </div>
+                                <div
+                                    class="flex flex-wrap w-full justify-center lg:justify-start lg:w-2/5 mt-2">
+                                    <?php foreach($product['variants'] as $productVariants){ ?>
+                                    <div class="">
+                                        <p
+                                            class="bg-primary_red py-2 px-3 mx-2 my-2 rounded-3xl hover:bg-transparent border border-primary_red hover:text-primary_red text-white">
+                                            <?php echo $productVariants['variant'] ?>
+                                        </p>
+                                    </div>
+                                    <?php }?>
+                                </div>
+                            </div>
+                            <?php if($product['bottom_border']=="show") {?>
+                            <div
+                                class="border-b-4 border-dashed border-primary my-14">
+                            </div>
+                            <?php }?>
+                            <?php } ?>
+                            <?php } ?>
+                        </div>
+                        <div class="w-full">
+                            <div class="w-full">
+                                <div
+                                    class='w-full flex flex-col <?php echo $image_content_v2['layout_direction']=="left"?"lg:flex-row":'lg:flex-row-reverse' ?> justify-evenly'>
+                                    <div
+                                        class="w-full lg:w-1/2 flex items-start justify-around flex-col ">
+                                        <?php foreach($image_content_v2['text_content'] as $item) {?>
+                                        <div class="mb-8">
+                                            <?php if($item['heading']) { ?>
+                                            <div>
+                                                <h2 class="text-2xl">
+                                                    <?php echo $item['heading'] ?>
+                                                </h2>
+                                            </div>
+                                            <?php } ?>
+                                            <?php if($item['paragraph']) { ?>
+                                            <div>
+                                                <p class="text-lg mt-2">
+                                                    <?php echo $item['paragraph'] ?>
+                                                </p>
+                                            </div>
+                                            <?php } ?>
+
+                                            <?php if($item['link']) { ?>
+                                            <div>
+                                                <div
+                                                    class="mt-6 px-4 py-3 rounded-3xl bg-primary_red">
+                                                    <a href="#">
+                                                        <?php echo $item['link']['title'] ?>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                            <?php } ?>
+                                        </div>
+                                        <?php } ?>
+                                    </div>
+                                    <div
+                                        class="w-full lg:w-2/5 flex flex-col items-center  <?php echo $image_content_v2['layout_direction']=="left"?"lg:items-end":'lg:items-start' ?>">
+                                        <?php foreach($image_content_v2['images'] as $images) {?>
+                                        <div class="w-full mb-4">
+                                            <img class="w-full"
+                                                src="<?php echo $images['image']['url'] ?>"
+                                                alt="" />
+
+                                        </div>
+                                        <?php } ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php if($image_content_v2['bottom_border']=="show") {?>
+                            <div
+                                class="border-b-4 border-dashed border-primary my-14">
+                            </div>
+                            <?php }?>
+
                         </div>
 
                         <div class="mt-10 px-10">
@@ -348,40 +520,101 @@
                         </div>
                     </div>
                     <div class="swiper-slide h-full flex flex-col">
+                        <div class="w-full">
+                            <div class="w-full">
+                                <div
+                                    class='w-full flex flex-col <?php echo $image_content_repeat['layout_direction']=="left"?"lg:flex-row":'lg:flex-row-reverse' ?> justify-evenly'>
+                                    <div
+                                        class="w-full lg:w-1/2 flex items-start justify-around flex-col ">
+                                        <?php foreach($image_content_repeat['text_content'] as $item) {?>
+                                        <?php if($item['heading']) { ?>
+                                        <div>
+                                            <h2 class="text-2xl">
+                                                <?php echo $item['heading'] ?>
+                                            </h2>
+                                        </div>
+                                        <?php } ?>
+                                        <?php if($item['paragraph']) { ?>
+                                        <div>
+                                            <p class="text-lg mt-6">
+                                                <?php echo $item['paragraph'] ?>
+                                            </p>
+                                        </div>
+                                        <?php } ?>
 
-                        <div class="mt-10 px-10">
-                            <div class="flex flex-wrap lg:justify-between">
-                                <div
-                                    class="w-full lg:w-30-percent lg:mb-0 mb-4">
-                                    <p class="italic text-2xl font-light">
-                                        “Fast and reliable service. More than
-                                        pleased with the final delivery. Thank
-                                        you Allan and the team.”
-                                    </p>
-                                    <p class="font-semibold mt-8">
-                                        Mike Parson
-                                    </p>
+                                        <?php if($item['link']) { ?>
+                                        <div>
+                                            <div
+                                                class="mt-6 px-4 py-3 rounded-3xl bg-primary_red">
+                                                <a href="#">
+                                                    <?php echo $item['link']['title'] ?>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <?php } ?>
+
+                                        <?php } ?>
+                                    </div>
+                                    <div
+                                        class="w-full lg:w-2/5 flex justify-center  <?php echo $image_content_repeat['layout_direction']=="left"?"lg:justify-end":'lg:justify-start' ?>">
+                                        <img class=""
+                                            src="<?php echo $image_content_repeat['image']['url'] ?>"
+                                            alt="" />
+                                    </div>
                                 </div>
-                                <div
-                                    class="w-full lg:w-30-percent lg:mb-0 mb-4">
-                                    <p class="italic text-2xl font-light">
-                                        “I was reluctant to order online but the
-                                        preview was fantastic and I am very
-                                        happy with the result.”
-                                    </p>
-                                    <p class="font-semibold mt-8">
-                                        Oliver
-                                    </p>
-                                </div>
-                                <div
-                                    class="w-full lg:w-30-percent lg:mb-0 mb-4">
-                                    <p class="italic text-2xl font-light">
-                                        “Pleasantly surprised by the service.
-                                        Highly recommend!”
-                                    </p>
-                                    <p class="font-semibold mt-8">
-                                        Adam Nielson
-                                    </p>
+                            </div>
+                            <?php if($image_content_repeat['bottom_border']=="show") {?>
+                            <div
+                                class="border-b-4 border-dashed border-primary my-14">
+                            </div>
+                            <?php }?>
+                            <div class="w-full">
+                                <div class="flex flex-col lg:flex-row">
+                                    <div class="w-full lg:w-45-percent">
+                                        <h3 class="text-2xl">
+                                            <?php echo $tables_image['heading'] ?>
+                                        </h3>
+                                        <p class="mt-8">
+                                            <?php echo $tables_image['paragraph'] ?>
+                                        </p>
+                                        <?php foreach($tables_image['tables'] as $table) { ?>
+
+                                        <table
+                                            class="mt-4 bg-white w-full border border-gray-200">
+                                            <tbody class="table-row-group  ">
+                                                <tr
+                                                    class="table-row table-row-first-styles bg-primary">
+                                                    <th
+                                                        class="text-left font-light text-white uppercase py-3 pl-2 pr-1 table-cell ">
+                                                        <?php echo $table['table_heading'] ?>
+                                                    </th>
+                                                    <th></th>
+                                                </tr>
+                                                <?php foreach($table['table_rows'] as $row) {?>
+                                                <tr
+                                                    class="table-row table-row-first-styles bg-transparent even:bg-secondary">
+                                                    <td
+                                                        class="text-left py-2 pl-2 pr-1 table-cell ">
+                                                        <?php echo $row['table_row_column_1'] ?>
+                                                    </td>
+                                                    <td
+                                                        class="text-left py-2 pl-2 pr-1 table-cell ">
+                                                        <?php echo $row['table_row_column_2'] ?>
+                                                    </td>
+                                                </tr>
+                                                <?php }?>
+                                            </tbody>
+                                        </table>
+                                        <?php }?>
+
+                                    </div>
+                                    <div
+                                        class="w-full lg:w-45-percent flex justify-end">
+                                        <div>
+                                            <img src="<?php echo $tables_image['image']['url'] ?>"
+                                                alt="">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -447,8 +680,6 @@
                                     <div class="mt-4">
                                         <p class="text-sm">
                                             A Rated Windows as standard
-
-
                                         </p>
                                     </div>
                                 </div>
