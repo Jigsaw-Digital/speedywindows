01a75dc986611b146d34bb4724a821d9987c0a6d
Angel-Jigsaw-Digital <88322644+Angel-Jigsaw-Digital@users.noreply.github.com>
2022-02-22 09:46:33 +0000

    header

--- wp-content/themes/sevenday/header.php
+++ wp-content/themes/sevenday/header.php
@@ -202,7 +202,7 @@ function navToggle() {
                                                     $sub_children = $child->wpse_children;
                                                 ?>
 
-                                                <div class="p-5">
+                                                <div class="mx-5">
                                                     <h6 class="uppercase ">
                                                         <a class="hover:text-secondary font-bold text-primary"
                                                             href="<?php echo $child_url ?>"><?php echo $child_title ?></a>
